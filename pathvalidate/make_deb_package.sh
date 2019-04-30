c795d128b04e43f99cad8c9e0e14c28acfb97d47
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-04-30 15:06:20 +0900

    Add pathvalidate

---
+++ pathvalidate/make_deb_package.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eux
+
+PKG=pathvalidate
+
+VERSION=$(python -c "import ${PKG}; print(${PKG}.__version__)")
+TAG=v${VERSION}
+ARCHIVE=${TAG}.tar.gz
+SRC_DIR=${PKG}-${VERSION}
+
+wget https://github.com/thombashi/${PKG}/archive/${ARCHIVE}
+tar -xvf ${ARCHIVE}
+mv ${ARCHIVE} ${PKG}_${VERSION}.orig.tar.gz
+cp -ar debian/ ${SRC_DIR}/
+cd ${SRC_DIR}/debian
+debuild -S -sa
