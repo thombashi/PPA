78580f20df6b73619b8a6378cadae1b0a78f57da
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-04-30 15:42:22 +0900

    Update scripts

--- pathvalidate/make_deb_package.sh
+++ pathvalidate/make_deb_package.sh
@@ -9,6 +9,7 @@ TAG=v${VERSION}
 ARCHIVE=${TAG}.tar.gz
 SRC_DIR=${PKG}-${VERSION}
 
+rm -rf ${SRC_DIR}
 wget https://github.com/thombashi/${PKG}/archive/${ARCHIVE}
 tar -xvf ${ARCHIVE}
 mv ${ARCHIVE} ${PKG}_${VERSION}.orig.tar.gz
